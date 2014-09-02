79e389d6ee231ae780fdf130edbf846f50157bc8
Fantasy Shao <forever.fantasy27@gmail.com>
2014-09-02 22:21:35 +0800

    Koa Application Started! :smile:

--- app.js
+++ app.js
@@ -7,42 +7,11 @@
  * Copyright (c) 2014 TeenWorks contributors
  */
 
-var path = require('path');
+var koa = require('koa');
+var app = koa();
 
-var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var MongoStore = require('connect-mongo')(session);
-
-require('./models');
-
-var routes = require('./routes');
-var config = require('./config').config;
-var staticDir = path.join(__dirname, 'public');
-
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(session({
-  secret: config.session_secret,
-  key: 'sid',
-  store: new MongoStore({
-    db: config.db_name
-  }),
-  resave: true,
-  saveUninitialized: true
-}));
-
-// set view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use('/public', express.static(staticDir));
-
-routes(app);
-
-app.listen(config.port, function() {
-  console.log('TeenShare listening on http://localhost:' + config.port);
+app.use(function *() {
+  this.body = 'hello koa';
 });
 
-module.exports = app;
+app.listen(3000);
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 var shell = require('gulp-shell');
 
-gulp.task('server', shell.task('node app.js'));
+gulp.task('server', shell.task('node --harmony app.js'));
 
 gulp.task('default', ['server']);
 
